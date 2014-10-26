76831ed65c3e67a5f31d8be361b7fe6be44b2aaf
Zac Lovoy <zwlovoy@gmail.com>
2014-10-26 05:40:57 +0000

    Favicon testing

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
 // Pull Riot API key from enviromental variables.
 apiKey = process.env.RIOTKEY;
