3e157bb4ec53ed949f8ba03f9197245e5585b244
Zac Lovoy <zwlovoy@gmail.com>
2014-10-26 05:31:54 +0000

    Favicon testing

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 var routes = require('./routes/index');
 var insult = require('./routes/insult');
 
@@ -17,8 +20,6 @@ apiKey = process.env.RIOTKEY;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
