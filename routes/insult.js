303e4972faa61844512c81267c998acf2cac52fb
Zac Lovoy <zwlovoy@gmail.com>
2014-11-01 05:50:30 +0000

    Testing cachine

--- routes/insult.js
+++ routes/insult.js
@@ -10,9 +10,9 @@ router.get('/', function(req, res) {
   // Get region and character name
   var region = req.query.region;
   var charName = req.query.charName;
-  console.log("Good Cache: " + myCache.get(charName.toLowerCase()+"|"+region));
+  console.log("Good Cache: " + myCache.get(charName.toLowerCase()+"|"+region).wins);
   console.log("-----------------");
-  console.log("Bad Cache: " + myCache.get("BLARG"));
+  console.log("Bad Cache: " + myCache.get("BLARG").wins);
   // Make request to turn charName into charID
   request(makeIDURL(charName, region), function (error, response, body) {
     // If a good response was returned
